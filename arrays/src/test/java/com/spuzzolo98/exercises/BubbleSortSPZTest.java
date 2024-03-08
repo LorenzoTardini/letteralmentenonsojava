371492d1f9143f50852eab71f019824111549ce3
Lorenzo <60008393+Spuzzolo98@users.noreply.github.com>
2024-03-08 15:58:01 +0100

    nonhofattonientediche

--- arrays/src/test/java/com/spuzzolo98/exercises/BubbleSortSPZTest.java
+++ arrays/src/test/java/com/spuzzolo98/exercises/BubbleSortSPZTest.java
@@ -1,16 +1,22 @@
 package com.spuzzolo98.exercises;
 
 import org.junit.jupiter.api.Test;
-
+import com.spuzzolo98.exercises.BubbleSortSPZ;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BubbleSortSPZTest {
 
     @Test
-    void bubbleSortCopy() {
-    }
+    public void bubbleSort() {
+        int[] v1 = {6, 3, 4, 2};
+        BubbleSortSPZ.bubbleSort(v1);
+        assertArrayEquals(new int[]{2, 3, 4, 6}, v1);
 
-    @Test
-    void bubbleSort() {
+        int[] v2 = {1, 0, 0, 0};
+        BubbleSortSPZ.bubbleSort(v2);
+        assertArrayEquals(new int[]{0, 0, 0, 1}, v2);
+
+        assertArrayEquals(new int[]{2, 3, 4, 6}, BubbleSortSPZ.bubbleSortCopy(new int[]{6, 3, 4, 2}));
+        assertArrayEquals(new int[]{0, 0, 0, 1}, BubbleSortSPZ.bubbleSortCopy(new int[]{1, 0, 0, 0}));
     }
 }
